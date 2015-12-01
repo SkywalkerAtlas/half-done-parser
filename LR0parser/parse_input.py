4274b31fc21af6b505451aff8a770a0755dfb5a1
SkywalkerAtlas <625741735@qq.com>
2015-12-02 00:05:47 +0800

    新增键入语句的模块

---
+++ LR0parser/parse_input.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'SkywalkerAtlas'
+
+def parse_input():
+    input_parse = raw_input('enter input:').strip()
+    return input_parse + "#"
+
+if __name__ == '__main__':
+    
+    inputed_parse = parse_input()
+    print inputed_parse
