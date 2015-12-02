c313a6c62da9673f163683de28853f8a489de59a
SkywalkerAtlas <625741735@qq.com>
2015-12-02 11:27:10 +0800

    新增判断是否为增广文法，若不是则扩展为增广文法(augmented grammer)

--- LR0parser/getBNF.py
+++ LR0parser/getBNF.py
@@ -10,12 +10,16 @@ def getBNF(file_name):
     each_grammer_strip_list = []
     
     with open(path.join(here, 'grammer',file_name)) as f:
-         for eachLine in f:
-             same_leftPart_strip_list = eachLine.strip().split('|')
-             grammer_productions_list.append(same_leftPart_strip_list[0])
-             left_part = same_leftPart_strip_list[0].split('->')[0] + '->'
-             for each_grammer in same_leftPart_strip_list[1:]:
-                 grammer_productions_list.append(left_part + each_grammer)
+        begin_production_left = f.readline().strip().split('->')[0]
+        if not begin_production_left.endswith('\''):
+            grammer_productions_list.append(begin_production_left+'->'+begin_production_left+'\'')
+        f.seek(0)
+        for eachLine in f:
+            same_leftPart_strip_list = eachLine.strip().split('|')
+            grammer_productions_list.append(same_leftPart_strip_list[0])
+            left_part = same_leftPart_strip_list[0].split('->')[0] + '->'
+            for each_grammer in same_leftPart_strip_list[1:]:
+                grammer_productions_list.append(left_part + each_grammer)
              
     return grammer_productions_list
 
