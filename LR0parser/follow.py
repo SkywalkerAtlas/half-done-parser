657140370644bdfa878ec6b891b5c6cda3003ab7
SkywalkerAtlas <625741735@qq.com>
2015-12-12 11:25:47 +0800

    更正了follow.py中的bug

--- LR0parser/follow.py
+++ LR0parser/follow.py
@@ -19,7 +19,7 @@ def get_follow_set(X,production_dist,terminals_list,non_terminals_list,follow,fi
 
 	for key in production_dist.keys():
 		for each_pro in production_dist[key]:
-			if each_pro.find(X) != -1:
+			if each_pro.find(X) != -1 and key != X:
 				if each_pro.endswith(X):
 					follow_set = follow_set | (get_follow_set(key,production_dist,terminals_list,non_terminals_list,follow,first))
 
@@ -61,5 +61,5 @@ def get_follow(file_name):
 	return follow
 
 if __name__ == '__main__':
-	output = get_follow('grammer.txt')
+	output = get_follow('grammer2.txt')
 	print output
